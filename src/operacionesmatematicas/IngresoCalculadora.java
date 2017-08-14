ceb205a104f538ce7efd01de3412e24a8e35e1a3
thewomaninred <thewomaninred@yahoo.com.ar>
2017-08-14 01:29:55 -0300

    Especializacion de pruebas unitarias para IngresoCalculadora Se contempla el caso del usuario tipo Docente y Alumno Dentro del tipo Alumno prueba si es Avanzao o Principiante

--- src/operacionesmatematicas/IngresoCalculadora.java
+++ src/operacionesmatematicas/IngresoCalculadora.java
@@ -13,17 +13,21 @@ public class IngresoCalculadora {
     private int nivelMinimo = 3;
 
     public Calculadora abrirCalcuadora(String tipoUsario, int nivel) {
-        //System.out.println("Ingresando a Calculadora "+tipoUsario+" nivel "+nivel);
+
         if (tipoUsario.equals("Docente")) {
             Calculadora calc = new Calculadora();
-             System.out.println("Calculadora para docente");
-             return calc;
-        } else if (nivel > nivelMinimo) {
-            Calculadora calc = new Calculadora();
-            System.out.println("Calculadora para alumno de nivel "+nivel);
+            //System.out.println("Calculadora para docente");
             return calc;
+        } else {
+            if (nivel > nivelMinimo) {
+                Calculadora calc = new Calculadora();
+                //System.out.println("Calculadora para alumno de nivel " + nivel);
+                return calc;
+            } else {
+                //System.out.println("Tu nivel no tiene acceso a la Calculadora");
+                return null;
+            }
         }
-        return null;
     }
 
 }
